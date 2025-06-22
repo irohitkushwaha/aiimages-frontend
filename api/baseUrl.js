4e89e3cc21b508fa18cb301489bd261f13b8fb3a
Rohit Kushwaha <rohitkushwaha.r780@gmail.com>
2025-06-22 21:17:01 +0530

    changed

--- api/baseUrl.js
+++ api/baseUrl.js
@@ -1,6 +1,6 @@
 // api/baseUrl.js//
 //github
-// const BASE_URL = "https://api.aigeneratedimagess.com/api" 
-const BASE_URL = "http://localhost:9000/api" 
+const BASE_URL = "https://api.aigeneratedimagess.com/api" 
+// const BASE_URL = "http://localhost:9000/api" 
 
 export default BASE_URL;
