49d477b21e2bae7df0422116d890e90c8c280eb4
Rohit Kushwaha <rohitkushwaha.r780@gmail.com>
2025-06-22 18:49:10 +0530

    changed metadata img

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,10 @@
-
 import HeroSection from "../components/features/herosection";
 import ImagesList from "@/components/features/imagesList";
 import { getHomepageImages } from "@/api/getHomepageImages";
 
 export async function generateMetadata() {
   return {
-    title: "Ai Generated Images", //
+    title: "Ai Generated Images",
     description:
       "Explore or Download High Quality Ai Generated Photos/Images/Pictures Collections for free and copyright/Royalty free Images",
     keywords:
@@ -17,13 +16,14 @@ export async function generateMetadata() {
       title: "AI Generated Images - Ultra Realistic Collections",
       description:
         "Explore or Download High Quality Ai Generated Photos/Images/Pictures Collections for free and copyright/Royalty free Images",
-      images: ["/AILOGO.png"],
+      images: ["https://aigeneratedimagess.com/AILOGO.png"],
     },
     twitter: {
       card: "summary_large_image",
       title: "AI Generated Images - Ultra Realistic Collections",
       description:
         "Explore or Download High Quality Ai Generated Photos/Images/Pictures Collections for free and copyright/Royalty free Images",
+      image: "https://aigeneratedimagess.com/AILOGO.png",
     },
   };
 }
@@ -31,7 +31,7 @@ export async function generateMetadata() {
 export default async function Home() {
   // Fetch initial images on the server (SSR)
   const initialImages = await getHomepageImages(0);
-  
+
   return (
     <div>
       <HeroSection />
