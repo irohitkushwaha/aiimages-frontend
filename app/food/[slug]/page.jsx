8ddc213d428727e94f01a306a320c6b9322ec15e
Rohit Kushwaha <rohitkushwaha.r780@gmail.com>
2025-06-26 23:40:47 +0530

    all slug img op

--- app/food/[slug]/page.jsx
+++ app/food/[slug]/page.jsx
@@ -2,18 +2,48 @@ import { getImageWithSimilar } from "@/api/getImageWithSimilar";
 import Button from "@/components/ui/button";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import Image from "next/image";
+
+const categoryMap = [
+  { fullCategory: "Business", shortCategory: "business" },
+  { fullCategory: "Finance", shortCategory: "finance" },
+  { fullCategory: "Education & Learning", shortCategory: "education" },
+  { fullCategory: "Technology", shortCategory: "technology" },
+  { fullCategory: "Festivals & occasions", shortCategory: "festivals" },
+  { fullCategory: "Fashion & beauty", shortCategory: "fashion" },
+  { fullCategory: "Travel, Lifestyle & Nature", shortCategory: "nature" },
+  {
+    fullCategory: "Home Design & Real Estate",
+    shortCategory: "real-estate",
+  },
+  { fullCategory: "Food & Drink", shortCategory: "food" },
+];
+
+function getShortCategory(fullCategory) {
+  const found = categoryMap.find((c) => c.fullCategory === fullCategory);
+  return found ? found.shortCategory : null;
+}
 
 export async function generateMetadata({ params }) {
   const { slug } = await params;
   const imageData = await getImageWithSimilar(slug);
 
+  const baseUrl = "https://aigeneratedimagess.com";
+  const shortCategory = getShortCategory(imageData.mainImage.Category);
+
+  const canonicalUrl = `${baseUrl}/${shortCategory}/${slug}`;
+
   return {
     title: imageData.mainImage.PageTitle,
     description: imageData.mainImage.PageDescription,
     robots: "index, follow",
+    alternates: {
+      canonical: canonicalUrl,
+    },
     openGraph: {
       title: imageData.mainImage.PageTitle,
       description: imageData.mainImage.PageDescription,
+      url: canonicalUrl,
       images: [
         {
           url: imageData.mainImage.ImageFile,
@@ -42,41 +72,32 @@ async function ImagePage({ params }) {
   const imageData = await getImageWithSimilar(slug);
 
   try {
-    const categoryMap = [
-      { fullCategory: "Business", shortCategory: "business" },
-      { fullCategory: "Finance", shortCategory: "finance" },
-      { fullCategory: "Education & Learning", shortCategory: "education" },
-      { fullCategory: "Technology", shortCategory: "technology" },
-      { fullCategory: "Festivals & occasions", shortCategory: "festivals" },
-      { fullCategory: "Fashion & beauty", shortCategory: "fashion" },
-      { fullCategory: "Travel, Lifestyle & Nature", shortCategory: "nature" },
-      {
-        fullCategory: "Home Design & Real Estate",
-        shortCategory: "real-estate",
-      },
-      { fullCategory: "Food & Drink", shortCategory: "food" },
-    ];
-
-    // Utility: Pass long category, get short category
-    function getShortCategory(fullCategory) {
-      const found = categoryMap.find((c) => c.fullCategory === fullCategory);
-      return found ? found.shortCategory : null;
-    }
     return (
       <div className="w-full py-2">
         <h1 className="text-center font-bold md:text-[32px] text-[27px] text-gray-900 py-[15px] capitalize px-[10px]">
           {imageData.mainImage.PageTitle}
         </h1>
         <div className="w-full mx-auto pt-[20px] px-[10px]">
-          <figure className="w-full mx-auto">
-            <img
+          <figure className="max-w-[1000px] mx-auto">
+            {/* <img
               src={imageData.mainImage.ImageFile}
               alt={imageData.mainImage.Alt}
               title={imageData.mainImage.ImgTitle}
               loading="lazy"
-              className="mx-auto"
+              className="w-full mx-auto"
               // className="w-full"
-            />
+            /> */}
+            <div className="relative" style={{ aspectRatio: "1000/630" }}>
+              <Image
+                src={imageData.mainImage.ImageFile}
+                alt={imageData.mainImage.Alt}
+                title={imageData.mainImage.ImgTitle}
+                fill
+                className="object-contain"
+                sizes="100vw"
+              />
+            </div>
+
             <figcaption className="text-center text-gray-800 text-[18px] font-semibold pt-[15px] pb-[30px] max-w-[1200px] mx-auto">
               {imageData.mainImage.Caption}
             </figcaption>
@@ -104,15 +125,25 @@ async function ImagePage({ params }) {
             >
               <Link
                 href={`/${getShortCategory(image.Category)}/${image.PageSlug}`}
-                rel="noopener noreferrer"
+                rel="noopener noreferrer relative"
                 className="block w-full"
+                style={{ aspectRatio: "4/3" }}
               >
-                <img
+                {/* <img
                   src={image.ImageFile}
                   alt={image.Alt}
                   title={image.ImgTitle}
                   className="w-full object-cover transition-transform duration-300 group-hover:scale-105"
                   loading="lazy"
+                /> */}
+
+                <Image
+                  src={image.ImageFile}
+                  alt={image.Alt}
+                  title={image.ImgTitle}
+                  fill
+                  className="object-cover transition-transform duration-300 group-hover:scale-105 rounded-md"
+                  sizes="(max-width: 768px) 100vw, 25vw"
                 />
 
                 {/* Hover overlay with title */}
