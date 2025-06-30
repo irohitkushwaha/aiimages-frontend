77211c937af50ddc2528d7b66c42fff0cb4f92f0
Rohit Kushwaha <rohitkushwaha.r780@gmail.com>
2025-06-30 11:04:51 +0530

    added strong

--- app/business/[slug]/page.jsx
+++ app/business/[slug]/page.jsx
@@ -147,16 +147,14 @@ async function ImagePage({ params }) {
                 />
 
                 {/* Hover overlay with title */}
-                {/* <div className="absolute inset-0 hover:bg-black/50 transition-all duration-300 flex items-center justify-center">
-                  <p className="text-white text-lg font-semibold text-center px-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 transform translate-y-4 group-hover:translate-y-0">
-                    {image.ImgTitle}
+                <div className="absolute inset-0 hover:bg-black/50 transition-all duration-300 flex items-center justify-center">
+                  <p>
+                    <strong className="text-white text-lg font-semibold text-center px-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 transform translate-y-4 group-hover:translate-y-0">
+                      {image.ImgTitle}
+                    </strong>
                   </p>
-                </divv> */}
-              
+                </div>
               </Link>
-              <figcaption className="absolute inset-0 hover:bg-black/50 transition-all duration-300 flex items-center justify-center text-white text-lg font-semibold text-center px-4 opacity-0 group-hover:opacity-100 transform translate-y-4 group-hover:translate-y-0">
-                {image.ImgTitle}
-              </figcaption>
             </figure>
           ))}
         </section>
