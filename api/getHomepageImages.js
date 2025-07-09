19ce6c168176319c5e6b5eda41ea82c0727db4d5
Rohit Kushwaha <rohitkushwaha.r780@gmail.com>
2025-07-09 14:07:01 +0530

    added error handling

--- api/getHomepageImages.js
+++ api/getHomepageImages.js
@@ -1,10 +1,31 @@
 import BASE_URL from "./baseUrl";
 
+// export async function getHomepageImages(page = 0) {
+//     const res = await fetch(`${BASE_URL}/image/homepage-images?page=${page}`, {
+//       cache: "no-store",
+//     });
+//     console.log("response for gethomepageimage", res)
+//     if (!res.ok) throw new Error("Failed to fetch images error from next js api error");
+//     const data = await res.json();
+//     return data.data;
+//   }
+
 export async function getHomepageImages(page = 0) {
+  try {
     const res = await fetch(`${BASE_URL}/image/homepage-images?page=${page}`, {
       cache: "no-store",
     });
-    if (!res.ok) throw new Error("Failed to fetch images error from next js api error");
+    console.log("response for gethomepageimage", res);
+    if (!res.ok) {
+      const errorText = await res.text();
+      throw new Error(
+        `Failed to fetch images. Status: ${res.status}. Message: ${errorText}`
+      );
+    }
     const data = await res.json();
     return data.data;
+  } catch (error) {
+    console.error("Error fetching homepage images:", error);
+    throw error; // Re-throwing the error so the caller can handle it.
   }
+}
